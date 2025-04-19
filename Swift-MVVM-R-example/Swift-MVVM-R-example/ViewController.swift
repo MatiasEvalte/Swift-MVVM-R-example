063d1adc1c3cf48cc982447e0b362f538b7a6b6f
Matias Borges Evalte <matiasevalte@hotmail.com>
2025-04-19 10:19:26 -0300

    feat: Criação do projeto.

---
+++ Swift-MVVM-R-example/Swift-MVVM-R-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-MVVM-R-example
+//
+//  Created by Matias Evalte on 19/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
